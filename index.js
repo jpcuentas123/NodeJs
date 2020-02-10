201a7b16f31294b0a6c8ecad9f26969fb96af5c1
jpcuentas123 <jpcuentas123@gmail.com>
2020-02-10 11:29:11 -0500

    EventsEmitter

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-const events = require('events');
-var eventEmitter = new events.EventEmitter();
-eventEmitter.on('tutorial', () => {
-  console.log('data received succesfully.');
-});
+const EventEmitter = require('events');
+var eventEmitter = new EventEmitter();
 
-eventEmitter.emit('tutorial');
+// eventEmitter.on('tutorial', (num1, num2) => {
+//     console.log(`Sum of ${num1} + ${num2} = ${num1 + num2}`);
+// });
+
+// eventEmitter.emit('tutorial', 7, 7);
+
+
+class Person extends EventEmitter {
+    constructor(name) {
+        super()
+
+        this._name = name
+    }
+
+    get name() {
+        return this._name
+    }
+}
+let pedro = new Person('Pedro')
+let christina = new Person('Christina')
+
+const printName = (action) => {
+    switch (action) {
+        case 'Christina':
+            console.log('My name is amazing: ' + christina.name);
+            break;
+
+        case 'Pedro':
+            console.log('My name is : ' + pedro.name);
+        break
+    }
+}
+
+pedro.on('name', printName)
+christina.on('name', printName)
+
+pedro.emit('name','Pedro')
+christina.emit('name','Christina')
