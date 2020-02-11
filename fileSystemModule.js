31340e5e67594eeb994b100cdbd594abe56f7c74
jpcuentas123 <jpcuentas123@gmail.com>
2020-02-11 10:38:47 -0500

    File System Module

---
+++ fileSystemModule.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+
+//Create a file
+
+// fs.writeFile('Example.json',`{
+//     "id":1,
+//     "title": "'Example post'",
+//     "content": "'My first Post blog'",
+//     "idiom": "'en'"
+// }`,error=>{
+//     if(error)
+//         console.log(error)
+//     else
+//         fs.readFile('Example.json','utf8',(error, file)=>{
+//             if(error)
+//                 console.log(error);
+//             else
+//                 console.log(file);
+//         })
+//         console.log('File successfully created')
+// })
+// fs.mkdir('DataBase', err => {
+//     if (err)
+//         console.log(err);
+//     else {
+//         fs.writeFile('Posts.json',`{
+//                 "id":1,
+//                 "title": "'Example post'",
+//                 "content": "'My first Post blog'",
+//                 "idiom": "'en'"
+//             }`,error=>{
+//                 if(error)
+//                     console.log(error)
+//                 else
+//                     console.log('File successfully created')
+//                     fs.rename('Posts.json', './DataBase/Posts.json', err => {
+//                         if (err)
+//                             console.log(err);
+//                         else
+//                             console.log('File moved and renamed');
+//                     })
+//             })
+//     }
+// })
+
+// fs.appendFile('Database/Posts.json','Post no such',err=>{
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log('Its Done');
+// })
+
+fs.unlink('./DataBase/Posts.json',err=>{
+    if(err)
+        console.log(err);
+    else
+        console.log('File is successfully deleted');
+})
